f82dedfee323b5cc5fbea3828e3ec4535bf246c0
emmylem <benjaminosondu55@gmail.com>
2023-10-28 21:20:25 +0100

    update

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -2,9 +2,9 @@ export default function createReportObject(employeesList) {
   return {
     allEmployees: {
       ...employeesList,
-  },
-  getNumberOfDepartments(employeesList) {
-    return Object.keys(employeesList).length;
-  },
- };
+    },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
 }
--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,3 +1,3 @@
-export default function returnHowManyArguments() {
- return theArgs.length;
+export default function returnHowManyArguments(...theArgs) {
+  return theArgs.length;
 }
